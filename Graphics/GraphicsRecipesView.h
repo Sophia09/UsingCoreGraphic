c55e26c64dbcd923a802251fd9e95cd13a456ed4
Sophia09 <lisai121121@gmail.com>
2014-08-14 10:59:32 +0800

    using core graphic to draw shapes and text

---
+++ Graphics/GraphicsRecipesView.h
@@ -0,0 +1,13 @@
+//
+//  GraphicsRecipesView.h
+//  Graphics
+//
+//  Created by thinkit  on 8/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphicsRecipesView : UIView
+
+@end
---
+++ Graphics/GraphicsRecipesView.m
@@ -0,0 +1,31 @@
+//
+//  GraphicsRecipesView.m
+//  Graphics
+//
+//  Created by thinkit  on 8/12/14.
+//
+//
+
+#import "GraphicsRecipesView.h"
+
+@implementation GraphicsRecipesView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
